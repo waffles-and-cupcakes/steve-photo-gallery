c35b598e5198a65551c4b76ee87e1ee9690a0d92
Steve Richards <steve.richards@gmail.com>
2018-04-12 19:45:49 -0700

    Create photo schema and model

---
+++ database/Photo.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const db = require('./index.js');
+mongoose.Promise = global.Promise;
+
+const photoSchema = new mongoose.Schema({
+  title: String,
+  isVerified: Boolean,
+  photoUrl: String,
+  listingId: Number,
+});
+
+const Photo = mongoose.model('Photo', photoSchema);
+
+module.exports = Photo;
