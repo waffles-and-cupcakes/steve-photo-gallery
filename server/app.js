00011d6ed062595916ef9e2daa32f99f0b19ca34
Steve Richards <steve.richards@gmail.com>
2018-04-23 17:32:30 -0700

    Allow cross site requests

--- server/app.js
+++ server/app.js
@@ -3,6 +3,12 @@ const db = require('../database/Photo');
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static('public'));
 
 app.get('/rooms/:id/photos', (req, res) => {
