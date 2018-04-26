a746599ad9c75ac082451f00b93286faa3adf30b
Steve Richards <steve.richards@gmail.com>
2018-04-26 13:26:28 -0700

    Add cors for ajax calls

--- server/app.js
+++ server/app.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const db = require('../database/Photo');
+const cors = require('cors');
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 app.use('/rooms/:id', express.static('public'));
 
